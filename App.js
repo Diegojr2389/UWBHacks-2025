5cd198301786a08320a017e7e0b920488b1314f8
ROMAN;) <romanavilezuw@gmail.com>
2025-04-26 01:45:09 -0700

    Add map functionality

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity, Modal } from 'react-native';
 import { useEffect, useState } from 'react';
 import * as Location from 'expo-location';
 import Events from './components/Events';
+import MapView, { Marker } from 'react-native-maps'
 
 export default function App() {
   const [location, setLocation] = useState();
@@ -24,17 +25,51 @@ export default function App() {
     })();
   }, []);
 
+  const [visible, setVisible] = useState(false);
+
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.mapButton}>
-        <Text style={styles.text}>Map</Text>
+        <Text style={styles.text} onPress={() => setVisible(true)}>Map</Text>
       </TouchableOpacity>
+
+      {/* Map popup */}
+      <Modal 
+        visible={visible}
+        animationType='slide'
+        onRequestClose={() => {setVisible(false)}}
+      >
+        <View style={styles.overlay}>
+          <TouchableOpacity style={styles.closeButton} onPress={() => {setVisible(false)}}>
+            <Text style={styles.closeBtnTxt}>CLOSE</Text>
+          </TouchableOpacity>
+          <View style={styles.popup}>
+            {location && (
+              <MapView
+              style={{flex: 1}}
+              region={{
+                latitude: location.latitude,
+                longitude: location.longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01
+              }}
+              >
+                <Marker
+                  coordinate={{ latitude: location.latitude, longitude: location.longitude}}
+                />
+              </MapView>
+            )}
+          </View>
+        </View>
+      </Modal>
     
       {/* {location ? (
         <Text>Lat: {location.latitude}, Lon: {location.longitude}</Text>
       ) : (
         <Text>Getting location...</Text>
       )} */}
+
+      {/* Event list */}
       <View style={styles.flatlist}>
         <Events data={data}/>
       </View>
@@ -65,5 +100,29 @@ const styles = StyleSheet.create({
   }, 
   flatlist: {
     marginTop: 100
+  },
+  closeButton: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#0e161b',
+    width: '100%'
+  },
+  closeBtnTxt: {
+    color: 'white',
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: '#56666f',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  popup: {
+    width: '95%',
+    height: '92%',
+    padding: 10,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    elevation: 20
   }
 });
