76313128d42fa04eff281825084ae3566c54efd4
Diegojr2389 <davilez062@gmail.com>
2025-04-25 17:48:48 -0700

    Add functionality to get user's location

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button, FlatList, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState();
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log("Permission denied!");
+        return;
+      }
+      let loc = await Location.getCurrentPositionAsync({});
+      setLocation(loc.coords);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity styles={styles.homeButton}>
-        <Text>Map</Text>
+      <TouchableOpacity style={styles.homeButton}>
+        <Text style={styles.text}>Map</Text>
       </TouchableOpacity>
+
+      {location ? (
+        <Text>Lat: {location.latitude}, Lon: {location.longitude}</Text>
+      ) : (
+        <Text>Getting location...</Text>
+      )}
+
       {/* <FlatList></FlatList> */}
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
@@ -23,6 +46,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   homeButton: {
-    backgroundColor: 'black'
+    position: 'absolute',
+    top: 35,
+    left: 10,
+    width: 50,
+    height: 30,
+    backgroundColor: 'black',
+    borderRadius: 5
+  },
+  text: {
+    color: 'white',
+    textAlign: 'center'
   }
 });
